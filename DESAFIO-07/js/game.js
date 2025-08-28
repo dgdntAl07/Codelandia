b1dad346071b7e23c4d710ba5700baef4fef8b31
dgdntAI07 <dgdntAl07>
2025-08-28 16:55:32 -0300

    jogo da memoria

---
+++ DESAFIO-07/js/game.js
@@ -0,0 +1,78 @@
+const grid = document.querySelector('.grid');
+
+const characters =[
+    'abobora01',
+    'abobora02',
+    'abobora03',
+    'abobora04',
+    'abobora05',
+    'abobora06',
+    'abobora07',
+    'abobora08',
+    'abobora09',
+];
+
+const createElement = (tag, className) => {
+
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+
+};
+
+let firstCard = '';
+let secondCard = '';
+
+
+const revealCard = ({target}) => {
+
+    if(target.parentNode.className.includes('reveal-card')){
+        return;
+    }
+
+    if (firstCard === ''){
+        target.parentNode.className.includes('reveal-card');
+        firstCard = target.parentNode;
+    } else if (secondCard){
+        target.parentNode.className.includes('reveal-card');
+        secondCard = target.parentNode;
+    }
+
+    target.parentNode.classList.add('reveal-card');
+};
+
+
+const createCard = (character) => {
+
+    const card = createElement('div' , 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../imgs/${character}.png')`;
+    // front.style.backgroundImage = "url('../imgs/"+character+".png')";
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+
+    return card;
+    
+};
+
+
+const loadGame = () => {
+
+    const duplicateCharacters = [ ... characters, ... characters];
+
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+    shuffledArray.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+
+    });
+
+};
+
+loadGame();
